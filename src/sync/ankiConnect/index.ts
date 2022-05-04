bea2eb9dbe85e291b3ad6851604856507c1494c2
Florian Pallas <mail@fpallas.com>
2022-05-04 20:32:06 +0200

    feat: allow cards to be deleted, recreated when known and moved

--- src/sync/ankiConnect/index.ts
+++ src/sync/ankiConnect/index.ts
@@ -5,6 +5,22 @@ import { Note } from './types';
 import { Notice } from 'obsidian';
 import { Article, Card } from '../../wiki';
 import { arrayBufferToBase64, stringToBase64 } from 'src/util';
+import {
+  AddNotesRequest,
+  CreateDeckRequest,
+  StoreMediaRequest,
+  UpdateNoteRequest,
+  NotesInfoRequest,
+  DeleteNotesRequest,
+  ChangeDeckRequest,
+} from './requests';
+
+interface CardRecord {
+  card: Card;
+  ankiNote: Note;
+  ankiMedia: [src: string, filePath: string][];
+  ankiCards?: number[];
+}
 
 export class AnkiConnectSyncService implements SyncService {
   private plugin: FlashcardsPlugin;
@@ -16,69 +32,146 @@ export class AnkiConnectSyncService implements SyncService {
   }
 
   public async push(articles: Article[]) {
-    const existingDecks = await this.bridge.getDecks();
+    new Notice('Pushing...');
+
+    console.time('getCards');
+    const [cardsToCreate, cardsToUpdate, cardsToDelete] = await this.getCards(
+      articles
+    );
+    console.log('creating', cardsToCreate.length);
+    console.log('updating', cardsToUpdate.length);
+    console.log('deleting', cardsToDelete.length);
+    console.timeEnd('getCards');
+
+    console.time('create-decks');
     const decksToCreate: string[] = [];
+    for (const { ankiNote } of cardsToCreate) {
+      if (decksToCreate.contains(ankiNote.deckName)) continue;
+      decksToCreate.push(ankiNote.deckName);
+    }
+    await this.bridge.sendMulti(
+      decksToCreate.map((deckName) => new CreateDeckRequest(deckName))
+    );
+    console.timeEnd('create-decks');
 
-    const cardsToCreate: [Card, Note][] = [];
-    const cardsToUpdate: [Card, Note][] = [];
+    console.time('create');
+    const newNoteIds = await this.bridge.send(
+      new AddNotesRequest(cardsToCreate.map(({ ankiNote }) => ankiNote))
+    );
+    for (let i = 0; i < newNoteIds.length; i++) {
+      const noteId = newNoteIds[i];
+      if (!noteId) {
+        console.log('failed to create card', cardsToCreate[i]);
+        continue;
+      }
+      this.plugin.labelMap.set(cardsToCreate[i].card.label, noteId);
+    }
+    console.timeEnd('create');
 
-    const mediaToUpdate: [src: string, filePath: string][] = [];
+    console.time('update');
+    await this.bridge.sendMulti(
+      cardsToUpdate.map(({ ankiNote }) => new UpdateNoteRequest(ankiNote))
+    );
+    await this.bridge.sendMulti(
+      cardsToUpdate.map(
+        ({ ankiNote }) =>
+          new ChangeDeckRequest([ankiNote.id || -1], ankiNote.deckName)
+      )
+    );
+    console.timeEnd('update');
+
+    console.time('update-media');
+    const mediaRequests = await Promise.all(
+      cardsToUpdate.flatMap(({ ankiMedia }) =>
+        ankiMedia.map(async ([src, filePath]) => {
+          const arrayBuffer = await this.plugin.app.vault.adapter.readBinary(
+            filePath
+          );
+          const data = arrayBufferToBase64(arrayBuffer);
+          return new StoreMediaRequest(src, data);
+        })
+      )
+    );
+    await this.bridge.sendMulti(mediaRequests);
+    console.timeEnd('update-media');
+
+    console.time('delete');
+    await this.bridge.send(
+      new DeleteNotesRequest(cardsToDelete.map(([, id]) => id))
+    );
+    for (const [label] of cardsToDelete) {
+      this.plugin.labelMap.delete(label);
+    }
+    console.timeEnd('delete');
+
+    new Notice(
+      [
+        'Done!',
+        `Scanned\t${articles.length} file(s)`,
+        `Created\t${cardsToCreate.length} card(s)`,
+        `Updated\t${cardsToUpdate.length} card(s)`,
+        `Deleted\t${cardsToDelete.length} card(s)`,
+      ].join('\n')
+    );
+  }
+
+  private async getCards(
+    articles: Article[]
+  ): Promise<[CardRecord[], CardRecord[], [string, number][]]> {
+    const existingCards: CardRecord[] = [];
+
+    const cardsToCreate: CardRecord[] = [];
+    const cardsToUpdate: CardRecord[] = [];
+    const cardsToDelete: [string, number][] = [];
 
     for (const article of articles) {
       for (const card of article.cards) {
-        const [note, media] = this.convertCard(card);
-        mediaToUpdate.push(...media);
+        const record = this.getRecord(card);
+        const { ankiNote } = record;
 
-        // Determine deck status
-        if (!existingDecks.contains(note.deckName)) {
-          decksToCreate.push(note.deckName);
-        }
-
-        // Determine note status
-        if (!note.id) {
-          cardsToCreate.push([card, note]);
+        if (!ankiNote.id) {
+          cardsToCreate.push(record);
           continue;
         }
-        cardsToUpdate.push([card, note]);
+        existingCards.push(record);
       }
     }
 
-    for (const deck of decksToCreate) {
-      await this.bridge.createDeck(deck);
-    }
-
-    const noteIds = await this.bridge.addNotes(
-      cardsToCreate.map(([card, note]) => note)
+    const cardInfos = await this.bridge.send(
+      new NotesInfoRequest(
+        existingCards.map(({ ankiNote }) => ankiNote.id || -1)
+      )
     );
 
-    for (let i = 0; i < noteIds.length; i++) {
-      const noteId = noteIds[i];
-      if (noteId) {
-        this.plugin.labelMap.set(cardsToCreate[i][0].label, noteId);
+    for (let i = 0; i < existingCards.length; i++) {
+      const record = existingCards[i];
+      const info = cardInfos[i];
+      if (!info) {
+        cardsToCreate.push(record);
+        continue;
+      }
+
+      if (
+        info.fields.Front.value !== record.ankiNote.fields.Front ||
+        info.fields.Back.value !== record.ankiNote.fields.Back
+      ) {
+        cardsToUpdate.push(record);
       }
     }
 
-    for (const [card, note] of cardsToUpdate) {
-      await this.bridge.updateNote(note);
+    const knownLabels = Array.from(this.plugin.labelMap.entries());
+    for (const [label, noteId] of knownLabels) {
+      if (
+        existingCards.findIndex(({ ankiNote }) => ankiNote.id === noteId) === -1
+      ) {
+        cardsToDelete.push([label, noteId]);
+      }
     }
 
-    for (const [src, filePath] of mediaToUpdate) {
-      const arrayBuffer = await this.plugin.app.vault.adapter.readBinary(
-        filePath
-      );
-      const data = arrayBufferToBase64(arrayBuffer);
-      await this.bridge.storeMediaFile(src, data);
-    }
-
-    const cardTotal = cardsToCreate.length + cardsToUpdate.length;
-    const cardActual = noteIds.length + cardsToUpdate.length;
-
-    new Notice(`Pushed ${cardActual}/${cardTotal} cards`);
+    return [cardsToCreate, cardsToUpdate, cardsToDelete];
   }
 
-  private convertCard(
-    card: Card
-  ): [note: Note, media: [src: string, filePath: string][]] {
+  private getRecord(card: Card): CardRecord {
     const id = this.plugin.labelMap.get(card.label);
 
     const rootDeck = this.plugin.settings.rootDeck;
@@ -89,6 +182,9 @@ export class AnkiConnectSyncService implements SyncService {
         (rootDeck.trim().length > 0 ? '/' : '') +
         card.article.file.parent.path;
       deckName = deckPath.replace(/\//g, '::');
+    }
+    if (deckName === '::') {
+      deckName = 'Default';
     }
 
     const media: [string, string][] = [];
@@ -130,6 +226,6 @@ export class AnkiConnectSyncService implements SyncService {
       },
     };
 
-    return [note, media];
+    return { card, ankiNote: note, ankiMedia: media };
   }
 }
