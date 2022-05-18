e249c8df44cc3f000ecf3c89fbe505dab6e1d3c4
Florian Pallas <mail@fpallas.com>
2022-05-18 15:10:07 +0200

    fix: escape math content to prevent backslashes getting removed

--- src/sync/ankiConnect/util.ts
+++ src/sync/ankiConnect/util.ts
@@ -4,6 +4,9 @@ import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
 import { stringToBase64 } from 'src/util';
 
+export const escapeMath = (content: string) =>
+  content.replace(/\\\\/g, '\\\\\\\\');
+
 export const processMarkdown = async (
   content: string,
   media: [string, string][]
@@ -17,8 +20,14 @@ export const processMarkdown = async (
         return `![${alt}](${src} "${title ?? ''}")`;
       }
     )
-    .replace(/\$\$(.*?)\$\$/gs, (_match, body: string) => `\\\\[${body}\\\\]`)
-    .replace(/\$(.*?)\$/g, (_match, body: string) => `\\\\(${body}\\\\)`);
+    .replace(
+      /\$\$(.*?)\$\$/gs,
+      (_match, body: string) => `\\\\[${escapeMath(body)}\\\\]`
+    )
+    .replace(
+      /\$(.*?)\$/g,
+      (_match, body: string) => `\\\\(${escapeMath(body)}\\\\)`
+    );
 
   const file = await unified()
     .use(remarkParse)
