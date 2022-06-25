46be0725083b73fd76fb20835d3f82f859074d54
Florian Pallas <mail@fpallas.com>
2022-06-25 20:38:00 +0200

    refactor: reset progress bar without transition when it is set to 0

--- src/gui/components/progress-bar.svelte
+++ src/gui/components/progress-bar.svelte
@@ -1,18 +1,31 @@
 <script lang="ts">
+  import { beforeUpdate } from 'svelte';
+
   export let value = 0;
+  let bar: HTMLSpanElement | undefined;
+
+  beforeUpdate(() => {
+    if (bar && value === 0) {
+      bar.style.transitionDuration = '0ms';
+      bar.style.width = '0%';
+      bar.offsetHeight;
+      bar.style.transitionDuration = '150ms';
+    }
+  });
 </script>
 
 <div class="progress-bar">
-  <span style="width: {value}%" />
+  <span style="width: {value}%" bind:this={bar} />
 </div>
 
 <style lang="scss">
   .progress-bar {
     position: relative;
     width: 100%;
-    height: 7.5px;
-    border-radius: 7.5px;
+    height: 5px;
+    border-radius: 2.5px;
     overflow: hidden;
+    background-color: var(--interactive-normal);
   }
 
   .progress-bar span {
@@ -21,6 +34,6 @@
     left: 0;
     height: 100%;
     background-color: var(--interactive-accent);
-    transition: width 150ms ease-out;
+    transition: width 150ms ease-in-out;
   }
 </style>
