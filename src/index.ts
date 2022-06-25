579fb7e0aa0e58c0567921e8aa50ed595a1ef19a
Florian Pallas <mail@fpallas.com>
2022-06-25 20:37:04 +0200

    refactor: overhaul plugin startup / shutdown logic

--- src/index.ts
+++ src/index.ts
@@ -1,181 +1,80 @@
 import log from 'loglevel';
-import { Plugin } from 'obsidian';
-import { SettingsTab } from './gui/settingsTab';
-import { Settings, SettingsLoader, LabelMapLoader } from './config';
-import { SyncService, NoneSyncService, AnkiConnectSyncService } from './sync';
-import { createLabel } from './util';
-import { parseWiki, Wiki } from './entities/wiki';
-import { transformWikiLinks as transformWikiLinks } from './transform/wikilinks';
-import View from './gui/sidebar';
+import { Notice, Plugin } from 'obsidian';
+import {
+  Settings,
+  readSettings,
+  writeSettings,
+  readLabels,
+  writeLabels,
+} from './config';
+import { FlashcardsView } from './gui/sidebar';
 
 export default class FlashcardsPlugin extends Plugin {
-  settingsLoader = new SettingsLoader(this);
-  labelMapLoader = new LabelMapLoader(this);
+  isInitialized = false;
 
   settings!: Settings;
-  labelMap!: Map<string, number>;
-  syncService!: SyncService;
+  labels!: Map<string, number>;
+  view!: FlashcardsView;
 
-  async onload() {
-    this.settings = await this.settingsLoader.load();
-    this.labelMap = await this.labelMapLoader.load(this.settings.labelMapPath);
-    this.onUpdate();
+  async initialize() {
+    if (this.isInitialized) {
+      log.warn('The plugin was already initialized');
+      return;
+    }
+    log.debug('Initializing...');
 
-    this.addCommand({
-      id: 'push',
-      name: 'Push flashcard changes',
-      callback: () => this.onScan(),
-    });
+    // Load settings and labels
+    this.settings = await readSettings(this);
+    log.debug('Loaded settings', this.settings);
+    this.labels = await readLabels(this.app.vault, this.settings.labelsPath);
+    log.debug('Loaded labels', this.labels);
 
-    this.addCommand({
-      id: 'create-label',
-      name: 'Create flashcard label',
-      editorCallback: (editor) => {
-        editor.replaceSelection(
-          editor.getSelection() + '^c-' + createLabel(this.labelMap)
-        );
-      },
-    });
+    // Apply settings
+    log.setLevel(this.settings.logLevel);
 
-    this.registerView('flashcards-overview', (leaf) => {
-      return new View(leaf, this);
-    });
-
-    this.addCommand({
-      id: 'test',
-      name: 'Test',
-      callback: () => {},
-    });
-
-    this.addCommand({
-      id: 'open-overview',
-      name: 'Flashcards overview',
-      callback: async () => {
-        if (
-          this.app.workspace.getLeavesOfType('flashcards-overview').length === 0
-        ) {
-          await this.app.workspace.getRightLeaf(false).setViewState({
-            type: 'flashcards-overview',
-          });
-        } else {
-          this.app.workspace
-            .getLeavesOfType('flashcards-overview')
-            .first()!
-            .detach();
-
-          await this.app.workspace.getRightLeaf(false).setViewState({
-            type: 'flashcards-overview',
-          });
-        }
-
-        this.app.workspace.revealLeaf(
-          this.app.workspace.getLeavesOfType('flashcards-overview').first()!
-        );
-      },
-    });
-
-    this.addSettingTab(new SettingsTab(this.app, this));
+    this.isInitialized = true;
+    log.debug('Initialized');
   }
 
-  onUpdate() {
-    switch (this.settings.syncProvider) {
-      case 'ankiConnect': {
-        this.syncService = new AnkiConnectSyncService(this);
-        break;
-      }
-      case 'none':
-      default: {
-        this.syncService = new NoneSyncService();
-        break;
-      }
-    }
+  async onload() {
+    // temporary adjust log level until settings are applied
+    log.setLevel('trace');
+
+    await this.initialize().catch((err) => {
+      new Notice(err);
+      log.error(err);
+    });
+    if (!this.isInitialized) return;
+
+    // this.addSettingTab(new SettingsTab(this.app, this));
+    this.registerView('flashcards', (leaf) => {
+      this.view = new FlashcardsView(leaf, this);
+      return this.view;
+    });
+
+    this.app.workspace.getLeavesOfType('flashcards').first()?.detach();
+    await this.app.workspace
+      .getRightLeaf(false)
+      .setViewState({ type: 'flashcards' });
+    this.app.workspace.revealLeaf(
+      this.app.workspace.getLeavesOfType('flashcards').first()!
+    );
   }
 
   async onunload() {
-    await this.save(false);
-  }
-
-  async onScan() {
-    log.debug('scanning...');
-
-    dispatchEvent(
-      new ProgressEvent('flashcards-progress', {
-        loaded: 0,
-        total: 5,
-      })
-    );
-
-    const wiki = await parseWiki(this.app.vault);
-    log.debug(wiki);
-
-    dispatchEvent(
-      new ProgressEvent('flashcards-progress', {
-        loaded: 1,
-        total: 5,
-      })
-    );
-
-    for (const article of wiki.articles) {
-      for (const card of article.cards) {
-        transformWikiLinks(wiki, card);
-      }
+    // do not persist settings or labels if the plugin was not successfully
+    // initialized, as the settings or labels might not be parsed correctly
+    // and therefore empty.
+    if (!this.isInitialized) {
+      log.warn('The plugin was not initialized. No data will be persisted.');
+      return;
     }
 
-    dispatchEvent(
-      new CustomEvent<{ wiki: Wiki }>('flashcards-update', { detail: { wiki } })
-    );
-
-    dispatchEvent(
-      new ProgressEvent('flashcards-progress', {
-        loaded: 2,
-        total: 5,
-      })
-    );
-
-    setTimeout(() => {
-      dispatchEvent(
-        new ProgressEvent('flashcards-progress', {
-          loaded: 3,
-          total: 5,
-        })
-      );
-    }, 500);
-
-    setTimeout(() => {
-      dispatchEvent(
-        new ProgressEvent('flashcards-progress', {
-          loaded: 4,
-          total: 5,
-        })
-      );
-    }, 750);
-
-    setTimeout(() => {
-      dispatchEvent(
-        new ProgressEvent('flashcards-progress', {
-          loaded: 5,
-          total: 5,
-        })
-      );
-    }, 1000);
-
-    log.debug('scan complete');
+    log.info('Saving settings and labels...');
+    await writeSettings(this, this.settings);
+    await writeLabels(this.app.vault, this.settings.labelsPath, this.labels);
   }
 
-  async onPush() {
-    log.debug('pushing...');
-
-    /*
-    await this.syncService.push(wiki.articles);
-    await this.save();
-    */
-
-    log.debug('push complete');
-  }
-
-  async save(shouldUpdate = true) {
-    await this.labelMapLoader.save(this.settings.labelMapPath, this.labelMap);
-    await this.settingsLoader.save(this.settings);
-    if (shouldUpdate) this.onUpdate();
-  }
+  onScan() {}
+  onPush() {}
 }
